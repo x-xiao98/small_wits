bd947c928a131d75ac7900a0081286bdfb9f830f
x_xiao98 <766245503@qq.com>
2024-03-29 11:53:35 +0800

    初始化工程

---
+++ wits-all/src/main/java/org/wits/Wits.java
@@ -0,0 +1,7 @@
+package org.wits;
+
+public class Wits {
+	public static void main(String[] args) {
+		System.out.println("你好 wits");
+	}
+}
---
+++ wits-all/src/test/java/IdUtilTest.java
@@ -0,0 +1,10 @@
+import org.junit.Test;
+import org.wits.core.util.IdUtil;
+
+public class IdUtilTest {
+	@Test
+	public void randomUUID(){
+		System.out.println(IdUtil.simpleUUID());
+
+	}
+}
